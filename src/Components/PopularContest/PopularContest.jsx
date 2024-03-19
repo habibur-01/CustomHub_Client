5ae706f25427437bb6e6a1c9912bbc2a12094e60
habibur-01 <h.r.habibur08@gmail.com>
2024-03-20 02:52:01 +0600

    manageusers page updated

--- src/Components/PopularContest/PopularContest.jsx
+++ src/Components/PopularContest/PopularContest.jsx
@@ -10,17 +10,23 @@ const PopularContest = () => {
 
     useEffect(() => {
         axiosSecure.get("/contest")
-            .then(response => setContestData(response.data))
+            .then(response => 
+                {if (Array.isArray(response.data)) {
+                setContestData(response.data);
+            } else {
+                // If not an array, convert to an array with a single element
+                setContestData([response.data]);
+            }})
             .catch(error => console.log(error))
     }, [])
-    // console.log(contestData)
+    console.log(Array.isArray(contestData) )
     return (
         <Container>
             <div>
                 <SectionHeader title={'Popular Contest'} subTitle={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, sed.'} />
                 <div className="grid lg:grid-cols-5 lg:gap-6 md:flex flex-wrap grid-cols-1 mx-10">
                     {
-                        contestData.slice(0, 5).map(contest => <Card key={contest.contestName} contest={contest} />)
+                        contestData.slice(0,4).map(contest => <Card key={contest.contestName} contest={contest} />)
                     }
                 </div>
 
--- src/Pages/Dashboard/Admin/ManageUsers.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers.jsx
@@ -4,18 +4,22 @@ import { Link } from "react-router-dom";
 
 
 const ManageUsers = () => {
-    const [allUser, setAllUser] = useState()
+    const [allUsers, setAllUsers] = useState([])
+    // const [totalUsers, setTotalUsers] = useState([])
 
     useEffect(() => {
         axiosSecure.get(`/users`)
             .then(res => {
-                setAllUser(res.data)
+                console.log(res.data)
+                setAllUsers(res.data)
             }).catch(err => {
                 console.log(err)
             })
 
     }, [])
-    console.log(allUser)
+
+    const users = allUsers.filter(data => data.role != "admin")
+    console.log(Array.isArray(users))
     const handleContestDelete = (id) => {
         axiosSecure.delete(`/users/${id}`)
             .then((response) => {
@@ -46,14 +50,16 @@ const ManageUsers = () => {
                         </tr>
                     </thead>
                     <tbody>
+                        {/* {totalUsers.length} */}
                         {/* row 1 */}
                         {
-                            allUser.length < 0 ? <tr className="row-span-7">
+                            users.length < 0 ? <tr className="row-span-7">
                                 <div className="flex h-[700px] justify-center items-center space-y-4">
                                     <h1 className="text-4xl font-bold">Here has not any usert</h1>
                                     
                                 </div>
-                            </tr> : allUser.map(data => <tr key={data._id}>
+                            </tr> : 
+                            users.map(data => <tr key={data._id}>
                                 <th>
                                     <label>
                                         <input type="checkbox" className="checkbox" />
@@ -72,11 +78,11 @@ const ManageUsers = () => {
 
                                     <span className="badge badge-ghost badge-sm">{data?.email}</span>
                                 </td>
-                                <td>{data?.price
+                                <td>{data?.role
                                 }</td>
                                 <th className="space-x-2">
                                     <button onClick={() => handleContestDelete(data._id)} className="btn btn-ghost text-white btn-sm text-xs bg-red-400">Delete</button>
-                                    <Link to={`/dashboard/manageuser/${data?._id}`} state={data}><button className="btn btn-ghost text-white btn-sm text-xs bg-[#646ffc]">Update</button></Link>
+                                    
                                 </th>
 
                             </tr>)
